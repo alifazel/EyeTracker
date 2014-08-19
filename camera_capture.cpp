614bafe43baf6174becd3839737fa5f6f491204d
Ali Fazel <alifazel.work@gmail.com>
2014-08-19 19:49:57 +0100

    Added start and stop camera capability to application

--- camera_capture.cpp
+++ camera_capture.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 int main ()
 {
+	// Initializes camera using start-up stream script
+	system("/home/pi/raspifastcam/start_camd.sh")
 	// Manually captures a frame when the program is called.
 	system("/home/pi/raspifastcam/do_capture.sh")
+	// Deinitializes camera using stop script
+	system("/home/pi/raspifastcam/stop_camd.sh")
 }
