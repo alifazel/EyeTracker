b5c4ec1298f5130e3a7cf5de08fa673286987dad
Ali Fazel <alifazel.work@gmail.com>
2014-08-19 18:31:11 +0100

    Implementation of the FindBlobs() algorithm

--- source.py
+++ source.py
@@ -3,14 +3,22 @@ from SimpleCV import *
 
 # Initialize the camera from a video file as variable 'cam' 
 cam = VirtualCamera("/home/parallels/Videos/example.mov", "video")
+display = Display()
 
-while True:									#Creates a Loop
-	img = cam.getImage()					#Extracts frame from video as variable 'img'
-	eye = img.findHaarFeatures("eye.xml")	#Find the Haar Features
+normaldisplay = True			# Boolean to switch between normal display and threshold
 
-	if eye is not None:				#Checks to see if the array is not Null
-		eyes = eyes.sortArea()		
-		myFace = eyes[-1]			#Checks the previous entry
-		myFace.draw(width=3, color=Color.RED)	#Draws a square around the detected eye
-		
-	img.show()
+while True:									# Creates a Loop
+	img = cam.getImage()					# Extracts frame from video as variable 'img'
+	dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2) # Finds Colour Distance from White (Grayscale)
+	segmented - dist.stretch(250,300)		# Applies Threshold between 200-300
+
+	blobs = segmented.findBlobs()			# Searches for blobs
+
+	if blobs:								# If Blobs are found...
+		radius = blobs[-1],radius()
+		img.drawCircle((blobs[-1].x, blobs[-1].y, blobs[-1].radius(), SimpleCV.Color.RED, radius))
+											# Draws Cricle around the blobs using the array blobs
+	if normaldisplay:			# If normalDisplay is true, then show the normal view
+		img.show()
+	else:						# Else if false show the threshold for debugging purposes
+		segmented.show()
