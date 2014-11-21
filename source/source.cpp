133c7cd552eb11f29afe20ba75a9ad90ce0b81b3
Ali Fazel <alifazel.work@gmail.com>
2014-11-21 17:48:39 +0000

    Changed to Open Video and Show. -Commented Algorithm

--- source/source.cpp
+++ source/source.cpp
@@ -7,34 +7,52 @@ using namespace std;
 
 int main()
 {
+	// Create an array to store the output vectors
 	vector<Vec3f> output_array;
 
+	// Initialise a video file and Error Handling
+	VideoCapture video("/home/pi/pupilDetect/vidtest.mp4");
+	if (!video.isOpened());
+	{
+		printf("Error Opening Video File \n");
+		return -1;
+	} 
+	else printf("The Video File has been opened correctly \n");
+
+	//Extract a frame from the video and display on screen.
 	Mat frame;
-	frame = imread("/home/pi/pupilDetect/sample.jpg")
 
-	// Image Processing
-	Mat gray_frame, thres_frame; 				// Creates two Mat variables to store transformations
-	int threshold_variable = 60;				// Threshold Variable that can be adjusted.
-
-	cvtColor(frame, gray_frame, CV_BGR2GRAY);	// Converts image to grayscale
-	threshold(gray_frame, thres_frame, threshold_variable, 255, 0);
-
-	// Analyses Image
-	HoughCircles(thres_frame, output_array, CV_HOUGH_GRADIENT, 2, thres_frame.rows/4, 70, 65);
-
-	// Draw Circle on result
-	for(int i=0; i<output_array.size(); i=++) {
-
-		// Defines the points and radius for the circle
-		Point center(cvRound(output_array[i][0]),cvRound(output_array[i][1]));
-		int radius = cvRound(output_array[i][2]);
-
-		// Draws the Circles
-		circle(gray_frame, center, 5, (255,0,255), -1, 8, 0); 		// Circle on Pupil
-		circle(gray_frame, center, radius, (255,0,255), 3, 8, 0);	// Circle on Center
+	for(::)
+	{
+		video >> frame;
+		imshow("frame", frame);
+		if (waitKey(30) >= 0) 
+			break;
 	}
 
-	imwrite("/home/pi/camera/output.jpg", gray_frame);	// Writes Output
+	return 0
+
+	// // Image Processing
+	// Mat gray_frame, thres_frame; 				// Creates two Mat variables to store transformations
+	// int threshold_variable = 60;				// Threshold Variable that can be adjusted.
+
+	// cvtColor(frame, gray_frame, CV_BGR2GRAY);	// Converts image to grayscale
+	// threshold(gray_frame, thres_frame, threshold_variable, 255, 0);
+
+	// // Analyses Image
+	// HoughCircles(thres_frame, output_array, CV_HOUGH_GRADIENT, 2, thres_frame.rows/4, 70, 65);
+
+	// // Draw Circle on result
+	// for(int i=0; i<output_array.size(); i=++) {
+
+	// 	// Defines the points and radius for the circle
+	// 	Point center(cvRound(output_array[i][0]),cvRound(output_array[i][1]));
+	// 	int radius = cvRound(output_array[i][2]);
+
+	// 	// Draws the Circles
+	// 	circle(gray_frame, center, 5, (255,0,255), -1, 8, 0); 		// Circle on Pupil
+	// 	circle(gray_frame, center, radius, (255,0,255), 3, 8, 0);	// Circle on Center
+	// }
 
 	return 0;
 	
