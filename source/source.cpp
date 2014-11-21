a195c7c629a137ea0dce1736dc1d252d77e49466
Ali Fazel <alifazel.work@gmail.com>
2014-11-21 18:17:38 +0000

    Added Image Processing to Video

--- source/source.cpp
+++ source/source.cpp
@@ -19,41 +19,37 @@ int main()
 	} 
 	else printf("The Video File has been opened correctly \n");
 
-	//Extract a frame from the video and display on screen.
+	// Extract a frame and Analyse
 	Mat frame;
 
 	for(::)
-	{
+	{	
+		// Extract Frame
 		video >> frame;
+
+		// Image Processing on Frame
+		cvtColor(frame, gray_frame, CV_BGR2GRAY);	
+		threshold(gray_frame, thres_frame, threshold_variable, 255, 0);
+
+		// Analyses for Circles
+		HoughCircles(thres_frame, output_array, CV_HOUGH_GRADIENT, 2, thres_frame.rows/4, 70, 65);
+
+		// Draw Circle on result
+		for(int i=0; i<output_array.size(); i=++) {
+
+			// Defines the points and radius for the circle
+			Point center(cvRound(output_array[i][0]),cvRound(output_array[i][1]));
+			int radius = cvRound(output_array[i][2]);
+
+			// Draws the Circles
+			circle(gray_frame, center, 5, (255,0,255), -1, 8, 0); 		// Circle on Pupil
+			circle(gray_frame, center, radius, (255,0,255), 3, 8, 0);	// Circle on Center
+		}
+
+		// Show Frame
 		imshow("frame", frame);
-		if (waitKey(30) >= 0) 
-			break;
+		if (waitKey(30) >= 0) break;
 	}
 
-	return 0
-
-	// // Image Processing
-	// Mat gray_frame, thres_frame; 				// Creates two Mat variables to store transformations
-	// int threshold_variable = 60;				// Threshold Variable that can be adjusted.
-
-	// cvtColor(frame, gray_frame, CV_BGR2GRAY);	// Converts image to grayscale
-	// threshold(gray_frame, thres_frame, threshold_variable, 255, 0);
-
-	// // Analyses Image
-	// HoughCircles(thres_frame, output_array, CV_HOUGH_GRADIENT, 2, thres_frame.rows/4, 70, 65);
-
-	// // Draw Circle on result
-	// for(int i=0; i<output_array.size(); i=++) {
-
-	// 	// Defines the points and radius for the circle
-	// 	Point center(cvRound(output_array[i][0]),cvRound(output_array[i][1]));
-	// 	int radius = cvRound(output_array[i][2]);
-
-	// 	// Draws the Circles
-	// 	circle(gray_frame, center, 5, (255,0,255), -1, 8, 0); 		// Circle on Pupil
-	// 	circle(gray_frame, center, radius, (255,0,255), 3, 8, 0);	// Circle on Center
-	// }
-
-	return 0;
-	
+	return 0;	
 }
